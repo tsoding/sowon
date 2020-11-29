463f374b3a451652e32c35dd29b57afaada7ff3c
rexim <reximkut@gmail.com>
2020-11-29 18:29:56 +0700

    Try to build with console subsystem instead of windows

--- build_msvc.bat
+++ build_msvc.bat
@@ -5,6 +5,6 @@ set CXXFLAGS=/std:c++17 /O2 /FC /W4 /WX /nologo
 set INCLUDES=/I SDL2\include
 set LIBS=SDL2\lib\x64\SDL2.lib SDL2\lib\x64\SDL2main.lib Shell32.lib
 
-cl.exe %CXXFLAGS% /Fepng2c png2c.c /link Shell32.lib -SUBSYSTEM:windows
+cl.exe %CXXFLAGS% /Fepng2c png2c.c /link Shell32.lib -SUBSYSTEM:console
 png2c.exe digits.png > digits.h
 cl.exe %CXXFLAGS% %INCLUDES% /Fesowon main.c /link %LIBS% -SUBSYSTEM:windows
