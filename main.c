594fe352ee698df37084b8beb74dc00008959140
raul <raul@users.noreply.git>
2024-06-08 10:36:18 +0200

    added struct Config to create eventLoop()

--- main.c
+++ main.c
@@ -40,6 +40,17 @@ typedef enum {
     MODE_CLOCK,
 } Mode;
 
+typedef struct Config {
+    Mode mode;
+    float displayed_time;
+    int paused;
+    int exit_after_countdown;
+    size_t wiggle_index;
+    float wiggle_cooldown;
+    float user_scale;
+    char prev_title[TITLE_CAP];
+} Config;
+
 
 
 // ERROR HANDLER
@@ -206,25 +217,6 @@ SDL_Texture *createTextureFromFile(SDL_Renderer *renderer)
 }
 
 int main(int argc, char **argv) {
-    Mode mode = MODE_ASCENDING;
-
-    float displayed_time = 0.0f;
-    int paused = 0;
-    int exit_after_countdown = 0;
-
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-p") == 0) {
-            paused = 1;
-        } else if (strcmp(argv[i], "-e") == 0) {
-            exit_after_countdown = 1;
-        } else if (strcmp(argv[i], "clock") == 0) {
-            mode = MODE_CLOCK;
-        } else {
-            mode = MODE_COUNTDOWN;
-            displayed_time = parse_time(argv[i]);
-        }
-    }
-
     secc(SDL_Init(SDL_INIT_VIDEO));
     secc(SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear"));
 
@@ -242,17 +234,46 @@ int main(int argc, char **argv) {
     SDL_Texture *digits = createTextureFromFile(renderer);
 
 
+   
+
+
+
+
+    // configuration 
+    Config config = {MODE_ASCENDING,  0.0f, 0, 0, 0, WIGGLE_DURATION, 1.0f, "hello world"};
+
     
+    // initial configuration
+    // Mode mode = MODE_ASCENDING;
+    // float displayed_time = 0.0f;
+    // int paused = 0;
+    // int exit_after_countdown = 0;
+    // 
+    // size_t wiggle_index = 0;
+    // float wiggle_cooldown = WIGGLE_DURATION;
+    // float user_scale = 1.0f;
+    // char prev_title[TITLE_CAP];
+
+
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-p") == 0) {
+            config.paused = 1;
+        } else if (strcmp(argv[i], "-e") == 0) {
+            config.exit_after_countdown = 1;
+        } else if (strcmp(argv[i], "clock") == 0) {
+            config.mode = MODE_CLOCK;
+        } else {
+            config.mode = MODE_COUNTDOWN;
+            config.displayed_time = parse_time(argv[i]);
+        }
+    }
+
+
 
     int quit = 0;
-    size_t wiggle_index = 0;
-    float wiggle_cooldown = WIGGLE_DURATION;
-    float user_scale = 1.0f;
-    char prev_title[TITLE_CAP];
-
     while (!quit) {
 
-        if (paused) {
+        if (config.paused) {
             secc(SDL_SetTextureColorMod(digits, PAUSE_COLOR_R, PAUSE_COLOR_G, PAUSE_COLOR_B));
         } else {
             secc(SDL_SetTextureColorMod(digits, MAIN_COLOR_R, MAIN_COLOR_G, MAIN_COLOR_B));
@@ -262,73 +283,74 @@ int main(int argc, char **argv) {
         SDL_Event event = {0};
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
-            case SDL_QUIT: {
-                quit = 1;
-            } break;
-
-            case SDL_KEYDOWN: {
-                switch (event.key.keysym.sym) {
-                case SDLK_SPACE: {
-                    paused = !paused;
+                case SDL_QUIT: {
+                    quit = 1;
                 } break;
 
-                case SDLK_KP_PLUS:
-                case SDLK_EQUALS: {
-                    user_scale += SCALE_FACTOR * user_scale;
-                } break;
+                case SDL_KEYDOWN: {
+                    switch (event.key.keysym.sym) {
+                    case SDLK_SPACE: {
+                        config.paused = !config.paused;
+                    } break;
 
-                case SDLK_KP_MINUS:
-                case SDLK_MINUS: {
-                    user_scale -= SCALE_FACTOR * user_scale;
-                } break;
+                    case SDLK_KP_PLUS:
+                    case SDLK_EQUALS: {
+                        config.user_scale += SCALE_FACTOR * config.user_scale;
+                    } break;
 
-                case SDLK_KP_0:
-                case SDLK_0: {
-                    user_scale = 1.0f;
-                } break;
+                    case SDLK_KP_MINUS:
+                    case SDLK_MINUS: {
+                        config.user_scale -= SCALE_FACTOR * config.user_scale;
+                    } break;
 
-                case SDLK_F5: {
-                    displayed_time = 0.0f;
-                    paused = 0;
-                    for (int i = 1; i < argc; ++i) {
-                        if (strcmp(argv[i], "-p") == 0) {
-                            paused = 1;
+                    case SDLK_KP_0:
+                    case SDLK_0: {
+                        config.user_scale = 1.0f;
+                    } break;
+
+                    case SDLK_F5: {
+                        config.displayed_time = 0.0f;
+                        config.paused = 0;
+                        for (int i = 1; i < argc; ++i) {
+                            if (strcmp(argv[i], "-p") == 0) {
+                                config.paused = 1;
+                            } else {
+                                config.displayed_time = parse_time(argv[i]);
+                            }
+                        }
+                        if (config.paused) {
+                            secc(SDL_SetTextureColorMod(digits, PAUSE_COLOR_R, PAUSE_COLOR_G, PAUSE_COLOR_B));
                         } else {
-                            displayed_time = parse_time(argv[i]);
+                            secc(SDL_SetTextureColorMod(digits, MAIN_COLOR_R, MAIN_COLOR_G, MAIN_COLOR_B));
+                        }
+                    } break;
+
+                    case SDLK_F11: {
+                        Uint32 window_flags;
+                        secc(window_flags = SDL_GetWindowFlags(window));
+                        if(window_flags & SDL_WINDOW_FULLSCREEN_DESKTOP) {
+                            secc(SDL_SetWindowFullscreen(window, 0));
+                        } else {
+                            secc(SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP));
+                        }
+                    } break;
+                    }
+                } break;
+
+                case SDL_MOUSEWHEEL: {
+                    if (SDL_GetModState() & KMOD_CTRL) {
+                        if (event.wheel.y > 0) {
+                            config.user_scale += SCALE_FACTOR * config.user_scale;
+                        } else if (event.wheel.y < 0) {
+                            config.user_scale -= SCALE_FACTOR * config.user_scale;
                         }
                     }
-                    if (paused) {
-                        secc(SDL_SetTextureColorMod(digits, PAUSE_COLOR_R, PAUSE_COLOR_G, PAUSE_COLOR_B));
-                    } else {
-                        secc(SDL_SetTextureColorMod(digits, MAIN_COLOR_R, MAIN_COLOR_G, MAIN_COLOR_B));
-                    }
                 } break;
 
-                case SDLK_F11: {
-                    Uint32 window_flags;
-                    secc(window_flags = SDL_GetWindowFlags(window));
-                    if(window_flags & SDL_WINDOW_FULLSCREEN_DESKTOP) {
-                        secc(SDL_SetWindowFullscreen(window, 0));
-                    } else {
-                        secc(SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP));
-                    }
-                } break;
-                }
-            } break;
-
-            case SDL_MOUSEWHEEL: {
-                if (SDL_GetModState() & KMOD_CTRL) {
-                    if (event.wheel.y > 0) {
-                        user_scale += SCALE_FACTOR * user_scale;
-                    } else if (event.wheel.y < 0) {
-                        user_scale -= SCALE_FACTOR * user_scale;
-                    }
-                }
-            } break;
-
-            default: {}
+                default: {}
             }
         }
+
         // INPUT END //////////////////////////////
 
         // RENDER BEGIN //////////////////////////////
@@ -337,53 +359,53 @@ int main(int argc, char **argv) {
         {
             int pen_x, pen_y;
             float fit_scale = 1.0;
-            initial_pen(window, &pen_x, &pen_y, user_scale, &fit_scale);
+            initial_pen(window, &pen_x, &pen_y, config.user_scale, &fit_scale);
 
-            const size_t t = (size_t) ceilf(fmaxf(displayed_time, 0.0f));
+            const size_t t = (size_t) ceilf(fmaxf(config.displayed_time, 0.0f));
 
             // TODO: support amount of hours >99
             const size_t hours = t / 60 / 60;
-            render_digit_at(renderer, digits, hours / 10,   wiggle_index      % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
-            render_digit_at(renderer, digits, hours % 10,  (wiggle_index + 1) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
-            render_digit_at(renderer, digits, COLON_INDEX,  wiggle_index      % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
+            render_digit_at(renderer, digits, hours / 10,   config.wiggle_index      % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
+            render_digit_at(renderer, digits, hours % 10,  (config.wiggle_index + 1) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
+            render_digit_at(renderer, digits, COLON_INDEX,  config.wiggle_index      % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
 
             const size_t minutes = t / 60 % 60;
-            render_digit_at(renderer, digits, minutes / 10, (wiggle_index + 2) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
-            render_digit_at(renderer, digits, minutes % 10, (wiggle_index + 3) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
-            render_digit_at(renderer, digits, COLON_INDEX,  (wiggle_index + 1) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
+            render_digit_at(renderer, digits, minutes / 10, (config.wiggle_index + 2) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
+            render_digit_at(renderer, digits, minutes % 10, (config.wiggle_index + 3) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
+            render_digit_at(renderer, digits, COLON_INDEX,  (config.wiggle_index + 1) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
 
             const size_t seconds = t % 60;
-            render_digit_at(renderer, digits, seconds / 10, (wiggle_index + 4) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
-            render_digit_at(renderer, digits, seconds % 10, (wiggle_index + 5) % WIGGLE_COUNT, &pen_x, &pen_y, user_scale, fit_scale);
+            render_digit_at(renderer, digits, seconds / 10, (config.wiggle_index + 4) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
+            render_digit_at(renderer, digits, seconds % 10, (config.wiggle_index + 5) % WIGGLE_COUNT, &pen_x, &pen_y, config.user_scale, fit_scale);
 
             char title[TITLE_CAP];
             snprintf(title, sizeof(title), "%02zu:%02zu:%02zu - sowon", hours, minutes, seconds);
-            if (strcmp(prev_title, title) != 0) {
+            if (strcmp(config.prev_title, title) != 0) {
                 SDL_SetWindowTitle(window, title);
             }
-            memcpy(title, prev_title, TITLE_CAP);
+            memcpy(title, config.prev_title, TITLE_CAP);
         }
         SDL_RenderPresent(renderer);
         // RENDER END //////////////////////////////
 
         // UPDATE BEGIN //////////////////////////////
-        if (wiggle_cooldown <= 0.0f) {
-            wiggle_index++;
-            wiggle_cooldown = WIGGLE_DURATION;
+        if (config.wiggle_cooldown <= 0.0f) {
+            config.wiggle_index++;
+            config.wiggle_cooldown = WIGGLE_DURATION;
         }
-        wiggle_cooldown -= DELTA_TIME;
+        config.wiggle_cooldown -= DELTA_TIME;
 
-        if (!paused) {
-            switch (mode) {
+        if (!config.paused) {
+            switch (config.mode) {
             case MODE_ASCENDING: {
-                displayed_time += DELTA_TIME;
+                config.displayed_time += DELTA_TIME;
             } break;
             case MODE_COUNTDOWN: {
-                if (displayed_time > 1e-6) {
-                    displayed_time -= DELTA_TIME;
+                if (config.displayed_time > 1e-6) {
+                    config.displayed_time -= DELTA_TIME;
                 } else {
-                    displayed_time = 0.0f;
-                    if (exit_after_countdown) {
+                    config.displayed_time = 0.0f;
+                    if (config.exit_after_countdown) {
                         SDL_Quit();
                         return 0;
                     }
@@ -392,7 +414,7 @@ int main(int argc, char **argv) {
             case MODE_CLOCK: {
                 time_t t = time(NULL);
                 struct tm *tm = localtime(&t);
-                displayed_time = tm->tm_sec
+                config.displayed_time = tm->tm_sec
                                + tm->tm_min  * 60.0f
                                + tm->tm_hour * 60.0f * 60.0f;
             } break;
